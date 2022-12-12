af4c1826c90f3bf3a01ca76bf681b25288c98468
NikolaLul <nikola.n.lulchev.2018@elsys-bg.org>
2022-12-12 20:55:10 +0200

    Add project files.

---
+++ Intermediate/Build/Win64/MyProject2Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30147
+#include "C:/Users/Niksata/Documents/Unreal Projects/MyProject2/Intermediate/Build/Win64/MyProject2Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
