ee31cbcd58fa27d8972c2230d9c596934c326429
NikolaLul <nikola.n.lulchev.2018@elsys-bg.org>
2023-01-24 15:24:00 +0200

    flipbook

---
+++ Source/MyProject2/Private/Enum_class.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Enum_class.h"
+
+Enum_class::Enum_class()
+{
+}
+
+Enum_class::~Enum_class()
+{
+}
---
+++ Source/MyProject2/Public/Enum_class.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class MYPROJECT2_API Enum_class
+{
+public:
+	Enum_class();
+	~Enum_class();
+};
