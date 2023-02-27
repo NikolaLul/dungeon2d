6900255a9f77c0959829bf0412d2008f645423c9
NikolaLul <nikola.n.lulchev.2018@elsys-bg.org>
2023-02-28 01:37:14 +0200

    health system

---
+++ Source/MyProject2/Public/MyEnemyPaperCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperCharacter.h"
+#include "MyEnemyPaperCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT2_API AMyEnemyPaperCharacter : public APaperCharacter
+{
+	GENERATED_BODY()
+	
+};
